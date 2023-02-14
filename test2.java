f2f94fc88812cd5f036551e4584ae45913590649
sahil kataria <sahilkataria.1989@gmail.com>
2023-02-14 15:32:55 +0530

    test

--- test2.java
+++ test2.java
@@ -1,5 +1,6 @@
-
-
+Copyright Notice: © 2013-2022 Silent Eight Pte. Ltd. or its affiliates. All rights reserved.
+ This file is subject to the intellectual property rights of Silent Eight Pte. Ltd. or its affiliates. Any use, redistribution, or copying by any means, in whole or in part, with or without modification, is prohibited without the prior written consent of Silent Eight.
+ Some portions contained in this file may be the copyrighted property of others or be subject to open-source licenses; acknowledgment of those copyrights is hereby given. All such material is used with the permission of the owner.
 public class HelloWorld {
 
     // method main(): ALWAYS the APPLICATION entry point
